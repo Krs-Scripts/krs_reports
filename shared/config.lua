75faeb0658923bb52845b931b5e724c20058529c
KRS <131356071+KRS-KAROS@users.noreply.github.com>
2023-07-23 15:37:32 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -4,5 +4,5 @@ Krs = {}
 Krs = {
     
     -- URL del webhook di Discord dove verranno inviate le segnalazioni
-    DiscordWebhook = 'https://discord.com/api/webhooks/1132248074132127836/TVgZ-ym-v7Q2I5OJZGiYpK_HpB5dLDU0Mddx4Y8yjw3qsHrfPB3k0fKwGdH66Hm0ON6Q',
+    DiscordWebhook = '',
 }
